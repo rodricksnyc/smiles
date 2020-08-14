61ddd1b9cb9250f079d1e47b6aff378082c53305
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-14 13:19:00 -0400

    stuff

--- js/index.js
+++ js/index.js
@@ -1,8 +1,25 @@
+jQuery(document).ready(function ($) {
+
 $('.cc-selector input').on("keyup", function (e) {
 
 	var code = (e.keyCode ? e.keyCode : e.which);
 	if (code == 9) {
 		$('.cc-selector input').css('-webkit-appearance', 'radio')
 	}
+
+})
+
+
+$("#Q4").on('keyup', function (event) {
+
+		var currentString = $("#Q4").val();
+		$("#div1").html(currentString.length);
+		if (currentString.length <= 2000) {  /*or whatever your number is*/
+				var intVal = 2000 - currentString.length;
+				$("#div1").html("(" + intVal + " characters left)" );
+		} else {
+				//do some different stuff with your div
+		}
+});
 
 })
