930f1d1f66dcc5b2f4df35a53256a0344dafaf54
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-17 10:01:17 -0400

    stuff

--- js/index.js
+++ js/index.js
@@ -14,8 +14,8 @@ $("#Q4").on('keyup', function (event) {
 
 		var currentString = $("#Q4").val();
 		$("#div1").html(currentString.length);
-		if (currentString.length <= 2000) {  /*or whatever your number is*/
-				var intVal = 2000 - currentString.length;
+		if (currentString.length <= 3000) {  /*or whatever your number is*/
+				var intVal = 3000 - currentString.length;
 				$("#div1").html("(" + intVal + " characters left)" );
 		} else {
 				//do some different stuff with your div
