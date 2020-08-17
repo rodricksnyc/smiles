40e66915bbbb7053fd3429f8ff786a5475daf47b
Elizabeth <elizabeth@Elizabeths-iMac.local>
2020-08-17 15:57:32 -0400

    stuff

--- js/index.js
+++ js/index.js
@@ -22,4 +22,17 @@ $("#Q4").on('keyup', function (event) {
 		}
 });
 
+
+$("a, button, input, [tabIndex='0']").on("keyup", function (e) {
+	var code = (e.keyCode ? e.keyCode : e.which);
+	if (code == 9) {
+		$(this).css('outline', 'dashed 3px #4599ff')
+	}
+
+})
+$("a, button, input, [tabIndex='0']").on('focusout', function() {
+	$(this).css('outline', 'none')
+})
+
+
 })
